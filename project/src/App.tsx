d7a6d9a958b0c6b9202fe2b11a55f06174982906
Omkar Kathile <omkarkathile420@gmail.comm>
2025-05-06 21:16:29 +0530

    new config

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -7,7 +7,7 @@ import Dashboard from './components/Dashboard';
 import AIAssistant from './components/AIAssistant';
 import StaffDashboard from './components/StaffDashboard';
 import ProtectedRoute from './components/ProtectedRoute';
-// import DrDashBoard from './components/DrDashBoard';
+//import DrDashBoard from './components/DrDashBoard';
 import PatientDetails from './components/PatientDetails';
 import DrDashBoard from './components/DrDashBoard';
 
